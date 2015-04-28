442581a3888ba7606025bdec339438937c6be9af
Adam Braus <ajbraus@gmail.com>
2015-04-28 15:24:07 -0700

    fixing

--- js/templates.js
+++ js/templates.js
@@ -2,3 +2,4 @@ $('#header').load('header.html');
 $('#footer').load('footer.html');
 $('#overview').load('portfolio-overview.html');
 $('.portfolio-tab').load('portfolio-risk-tab.html');
+$('#becomeAMember').load('become-a-member.html');
